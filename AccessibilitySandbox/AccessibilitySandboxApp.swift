35a171321815123d7f9e9f8962c6b12929985e95
sizaan5 <sizaan55@gmail.com>
2025-02-03 16:06:10 +0500

    Project created

---
+++ AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Izaan Saleem on 03/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
