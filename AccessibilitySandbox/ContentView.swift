793f3992193b4d77a68b61e576debdefbf4d1939
Izaan Saleem <izaan.saleem@m3tech.com.pk>
2025-03-13 11:26:15 +0500

    Day 74/100

--- AccessibilitySandbox/ContentView.swift
+++ AccessibilitySandbox/ContentView.swift
@@ -9,9 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     
+    let pictures = [
+        "ales-krivec-15949",
+        "galina-n-189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096",
+    ]
+    
+    let labels = [
+        "Tulips",
+        "Frozen tree buds",
+        "Sunflowers",
+        "Fireworks"
+    ]
+    
+    @State private var selectedPicture = Int.random(in: 0...3)
+    
     @State private var value = 10
     
     var body: some View {
+        
+        VStack {
+            Button {
+                selectedPicture = Int.random(in: 0...3)
+            } label: {
+                Image(pictures[selectedPicture])
+                    .resizable()
+                    .scaledToFit()
+            }
+            .padding()
+            .accessibilityLabel(labels[selectedPicture])
+            .accessibilityAddTraits(.isButton)
+            .accessibilityRemoveTraits(.isImage)
+        }
+        
+        VStack {
+            Text("Your score is")
+            Text("1000")
+                .font(.title)
+        }
+        .padding()
+        .accessibilityElement()
+        .accessibilityLabel("Your score is 1000")
+        
         VStack {
             Text("Value: \(value)")
             
